44d2a4da3b0bcc3030c60fac08f9947ae74d7cfd
zhaiyundong <dongDONG!@#>
2026-05-11 11:14:55 +0800

    添加萤石sdk

--- app/src/main/java/com/yiman/ad/adbid/platform/PlatformManager.java
+++ app/src/main/java/com/yiman/ad/adbid/platform/PlatformManager.java
@@ -30,6 +30,7 @@ public class PlatformManager {
             mItemList.add(new ItemModel("Sigmob", true, AdBidPlatform.Sigmob.getLabel()));
             mItemList.add(new ItemModel("美数", true, AdBidPlatform.MS.getLabel()));
             mItemList.add(new ItemModel("Funlink", true, AdBidPlatform.FL.getLabel()));
+            mItemList.add(new ItemModel("萤石", true, AdBidPlatform.Ezviz.getLabel()));
         }
         setConfig();
     }
