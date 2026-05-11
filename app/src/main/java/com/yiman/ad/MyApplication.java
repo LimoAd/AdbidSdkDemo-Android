a0e757fcc237b324b06964c993d8c9ca8e85e1a0
zhaiyundong <dongDONG!@#>
2026-05-11 10:31:25 +0800

    删除无用字段

--- app/src/main/java/com/yiman/ad/MyApplication.java
+++ app/src/main/java/com/yiman/ad/MyApplication.java
@@ -31,12 +31,8 @@ public class MyApplication extends Application {
                 .setAppChannel("xiaomi")
                 //设置App版本
                 .setAppVersion("1.0.0")
-                //设置年龄
-                .setAge(18)
                 //设置用户ID
                 .setUserId("xxxxxx")
-                //设置用户性别，Male表示男性，Female表示女性
-                .setGender(AdbidInitConfig.AdbidGender.Female)
                 //设置隐私权限
                 .addCustomController(new AdbidCustomController() {
                     //是否允许SDK主动使用手机硬件参数（如IMEI）
